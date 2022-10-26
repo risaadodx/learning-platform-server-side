7f3587ce66c2c606c0ea0d21457b35393ca36e8c
risaadodx <wrisaad@gmail.com>
2022-10-26 12:59:36 +0600

    category and course json file created

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.port || 5000;
 
 app.use(cors());
 const categories = require("./data/Categories.json");
+const course = require("./data/Course.json");
 
 app.get("/", (req, res) => {
   res.send("courses api running");
@@ -12,6 +13,17 @@ app.get("/", (req, res) => {
 
 app.get("/courses-categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  const category_course = course.filter((n) => n.category_id === id);
+  res.send(category_course);
+});
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = course.find((n) => n._id === id);
+  res.send(selectedCourse);
 });
 
 app.listen(port, () => console.log("courses server running on port ", port));
